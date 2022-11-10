56ff554066d5234c3b182f714c85386bf0944ac4
gitfrancisJ <pelaezfrancisjohn@gmail.com>
2022-11-10 14:32:44 +0800

    added new component: Clock.vue for displaying current date and time

---
+++ components/Clock.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="flex flex-row gap-2">
+      <h1>{{ currentDay }} -</h1>
+      <h1>{{ currentMonth }}</h1>
+      <h1>{{ currentDate }},</h1>
+      <h1>{{ currentYear }} -</h1>
+      <h1>{{ currentTime }}</h1>
+    </div>
+  </template>
+  
+<script>
+export default {
+
+  created() {
+    setInterval(()=>{
+      this.getTime(); 
+    },1000)
+  },
+
+
+  data() {
+    return {
+      currentTime: '',
+      currentDay: '',
+      currentDate: '',
+      currentMonth: '',
+      currentYear: '',
+    }
+  },
+
+  methods: {
+    getTime() {
+        const dayToday = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+        const monthToday = ['JAN','FEB', 'MARCH', 'APRIL', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+        const today = new Date();
+        const time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+        const day = today.getDay();
+        const thisMonth = today.getMonth();
+        this.currentMonth = monthToday[thisMonth];
+        this.currentDate = today.getDate();
+        this.currentDay = dayToday[day];
+        this.currentTime = time;
+        this.currentYear = today.getFullYear();
+      }
+  }
+}
+  </script>
