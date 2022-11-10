2e8ab60f4073351c4d1457df6e5c52c03ee46714
gitfrancisJ <pelaezfrancisjohn@gmail.com>
2022-11-10 12:23:40 +0800

    changed the logout label to login

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,9 +2,6 @@
     <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
       <div class="flex text-white mr-6">
         <div class="h-10 w-40 pt-2">
-          <!-- <nuxt-link to="/">
-            <img class ="h-10 w-40" src="https://www.dreamarchers.com.ph/wp-content/uploads/2020/05/dream-archers-logo.svg" alt="">
-          </nuxt-link> -->
           <Logo />
         </div>
       </div>
@@ -13,17 +10,8 @@
           <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
         </button>
       </div>
-      <div class="hidden w-full block flex-grow lg:flex lg:items-center lg:w-auto" id="menu">
+      <div class="hidden w-full block flex-grow lg:flex lg:items-center lg:w-auto z-{2} " id="menu">
         <div class="text-sm lg:flex-grow">
-          <!-- <nuxt-link to="about" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-            My Logs
-          </nuxt-link>
-          <nuxt-link to="about" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-            Examples
-          </nuxt-link>
-          <nuxt-link to="about" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white">
-            Blog
-          </nuxt-link> -->
           <Menu>
             <menu-item
               v-for="item in navigations"
@@ -36,7 +24,7 @@
         </div> 
         <div>
           <nuxt-link to="about" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">
-            Login
+            Logout               
           </nuxt-link>
         </div>
       </div>
