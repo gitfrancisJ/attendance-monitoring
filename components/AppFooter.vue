d9eedd38f40b189d603c643c10b14d09199b83cd
gitfrancisJ <pelaezfrancisjohn@gmail.com>
2022-11-11 10:54:38 +0800

    added logo in the footer and reserve label

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>
-        
-    </div>
+    <footer class="p-4 bg-white sm:p-6 bg-slate-100">
+        <div class="flex items-center justify-center">
+            <Logo />
+        </div>
+        <hr class="border-gray-200 dark:border-gray-700">
+        <div class="flex items-center justify-center">
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 DreamArchers. All Rights Reserved.
+            </span>
+        </div>
+    </footer>
 </template>
