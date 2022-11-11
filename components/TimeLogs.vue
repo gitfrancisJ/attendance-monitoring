f15d8b7cd730509aa34de29797022248c05a7218
gitfrancisJ <pelaezfrancisjohn@gmail.com>
2022-11-11 15:52:52 +0800

    make the data in the select element dynamic and added a function to get the value of the selected option

--- components/TimeLogs.vue
+++ components/TimeLogs.vue
@@ -17,17 +17,60 @@
                     transition
                     ease-in-out
                     m-0
-                    focus:text-gray-700 focus:bg-white focus:border-teal-500 focus:outline-none" aria-label=".form-select-sm">
-                    <option disabled selected>--Select an action--</option>
-                    <option value="1">Login</option>
-                    <option value="2">Morning Break</option>
-                    <option value="3">Lunch Break</option>
-                    <option value="4">Afternoon Break</option>
-                    <option value="5">Logout</option>
+                    focus:text-gray-700 focus:bg-white focus:border-teal-500 focus:outline-none" aria-label=".form-select-sm"
+                    id="selectId">
+                    <option v-for="data in userActions" :value="data.value" :key="data.id">
+                        {{data.label}}
+                    </option>
                     </select>
-                    <button class="leading-none border rounded text-white border-white hover:border-transparent bg-teal-500 text-sm p-2 ">Submit</button>
+                    <button class="leading-none border rounded text-white border-white hover:border-transparent bg-teal-500 text-sm p-2 "
+                    @click.prevent="getValue()"
+                    >Submit
+                    </button>
                 </div>
             </div>
         </form>
     </div>
 </template>
+<script>
+export default {
+    data() {
+        return {
+            userActions : [
+                {
+                    id: 0,
+                    value: 'login',
+                    label: 'Login',
+                },
+                {
+                    id: 1,
+                    value: 'morning-break',
+                    label: 'Morning Break',
+                },
+                {
+                    id: 2,
+                    value: 'lunch-break',
+                    label: 'Lunch Break',
+                },
+                {
+                    id: 3,
+                    value: 'afternoon-break',
+                    label: 'Afternoon Break',
+                },
+                {
+                    id: 4,
+                    value: 'logout',
+                    label: 'Logout',
+                },
+            ]
+        }
+    },
+
+    methods: {
+        getValue() {
+           let data = document.getElementById('selectId');
+           console.log(data.value);
+        }
+    }
+}
+</script>
