80945b847c31c60038e6f310f28bee46e1e68319
gitfrancisJ <pelaezfrancisjohn@gmail.com>
2022-11-11 15:51:30 +0800

    removed the heading

--- components/MainPage.vue
+++ components/MainPage.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="flex items-center flex-col justify-center h-screen gap-10">
-        <h1 class="text-center">User Log</h1>
         <Clock/>
         <TimeLogs />
     </div>
