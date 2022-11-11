5c9d8ab50e1d4660ca8a11066c43a0f67f2bd876
gitfrancisJ <pelaezfrancisjohn@gmail.com>
2022-11-11 10:35:51 +0800

    renamed the imported component from DateTime to Clock

--- components/MainPage.vue
+++ components/MainPage.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex items-center flex-col justify-center h-screen gap-10">
         <h1 class="text-center">User Log</h1>
-        <DateTime />
+        <Clock/>
         <TimeLogs />
     </div>
 </template>
