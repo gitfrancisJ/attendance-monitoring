6757d12e1fcadcd5084c3d718d7131dad64c727f
gitfrancisJ <pelaezfrancisjohn@gmail.com>
2022-11-15 13:15:35 +0800

    fixed the code spacing

--- components/TimeLogs.vue
+++ components/TimeLogs.vue
@@ -1,23 +1,21 @@
 <template>
-    <div class="w-1/2 h-1/2 border-2 border-teal-500 flex items-center justify-center">
+    <div class="w-1/2 h-1/2 border-2 border-teal-500 flex items-center justify-center shadow-xl">
         <form>
             <div class="flex justify-center">
                 <div class="mb-3 xl:w-96 flex flex-col justify-center gap-20">
-                    <select class="form-select form-select-sm
-                    block
-                    w-full
-                    px-2
-                    py-1
-                    text-sm
-                    font-normal
-                    text-gray-700
-                    bg-white bg-clip-padding bg-no-repeat
-                    border border-solid border-gray-300
-                    rounded
-                    transition
-                    ease-in-out
-                    m-0
-                    focus:text-gray-700 focus:bg-white focus:border-teal-500 focus:outline-none" aria-label=".form-select-sm"
+                    <select 
+                    class="
+                        form-select form-select-sm block w-full px-2 py-1
+                        text-sm font-normal text-gray-700
+                        bg-white bg-clip-padding bg-no-repeat
+                        border border-solid border-gray-300
+                        rounded
+                        transition
+                        ease-in-out
+                        m-0
+                        focus:text-gray-700 focus:bg-white focus:border-teal-500 focus:outline-none
+                    "
+                    aria-label=".form-select-sm"
                     id="selectId">
                     <option v-for="data in userActions" :value="data.value" :key="data.id">
                         {{data.label}}
