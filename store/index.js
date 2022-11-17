000e798feec0fe6d72a99c60ceceb8654b90b51e
gitfrancisJ <pelaezfrancisjohn@gmail.com>
2022-11-17 16:50:34 +0800

    added functionalities and state for toggling.

--- store/index.js
+++ store/index.js
@@ -1,15 +1,21 @@
 export const state = () => ({
-   
+
+  userCheckIn: false
+
   });
 
   export const getters = () => ({
-   
+   isCheckIn: (state) => state.userCheckIn,
 });
 
 export const mutations = () => ({
-   
+  IS_CHECKEDIN(state) {
+    state.userCheckIn = true;
+  },
 });
 
 export const actions = () => ({
-   
+  checkedUser({ commit }) {
+    commit("IS_CHECKEDIN");
+  },
 });
