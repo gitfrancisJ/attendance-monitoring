7d0f594b253ca44cfb2b481b5e3a56a3bdc800cf
gitfrancisJ <pelaezfrancisjohn@gmail.com>
2022-11-17 15:44:45 +0800

    created a reusable button

---
+++ components/_AppButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+   <button class="w-full bg-teal-500 text-sm p-2 text-white rounded disabled:bg-gray-300" :disabled="btn.btnStatus" >
+    {{btn.label}}
+   </button>
+  </div>
+</template>
+<script>
+export default {
+    props: {
+        btn: {
+        type: Object,
+        default: () => {},
+        }
+    }
+}
+</script>
