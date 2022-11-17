7b9eb60e20d82448ceb20f0ab214abdf9d4beeb7
gitfrancisJ <pelaezfrancisjohn@gmail.com>
2022-11-17 16:51:46 +0800

    added methods for toggling buttons

--- components/_AppButton.vue
+++ components/_AppButton.vue
@@ -1,17 +1,33 @@
 <template>
   <div>
-   <button class="w-full bg-teal-500 text-sm p-2 text-white rounded disabled:bg-gray-300" :disabled="btn.btnStatus" >
+   <button class="w-full bg-teal-500 text-sm p-2 text-white rounded" @click.prevent='onClick(btn.method)' >
     {{btn.label}}
    </button>
   </div>
 </template>
 <script>
+import { mapActions } from 'vuex';
 export default {
-    props: {
-        btn: {
-        type: Object,
-        default: () => {},
-        }
+  props: {
+    btn: {
+      type: Object,
+      default: () => {},
     }
+  },
+
+  data() {
+    return {
+      userCheckedIn: false,
+    }
+  },
+
+  methods: {
+
+    ...mapActions(['checkedUser']),
+
+    onClick(methodName) {
+          this[methodName]();
+        }
+  }
 }
 </script>
