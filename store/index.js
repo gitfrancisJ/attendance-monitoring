137e689e1fddb7dbce55747d17b553f395d84347
gitfrancisJ <pelaezfrancisjohn@gmail.com>
2022-11-24 16:13:56 +0800

    added a state to determine if the user has successfully logged their credentials into the system

--- store/index.js
+++ store/index.js
@@ -1,21 +1,54 @@
 export const state = () => ({
 
-  userCheckIn: false
+  user: false,
+  userCheckIn: true,
+  userBreak: false,
 
   });
 
-  export const getters = () => ({
-   isCheckIn: (state) => state.userCheckIn,
-});
+export const getters = {
+  isCheckIn: (state) => state.userCheckIn,
+  isOnBreak: (state) => state.userBreak
+};
 
-export const mutations = () => ({
-  IS_CHECKEDIN(state) {
-    state.userCheckIn = true;
+export const mutations = {
+
+  ON_AUTH_STATE_CHANGED_MUTATION: (state, { authUser, claims }) => {
+    if (authUser) {
+      const { uid, email } = authUser;
+      state.user = { uid, email };
+    } else {
+      state.user = false;
+    }
   },
-});
 
-export const actions = () => ({
+  IS_CHECKEDIN(state) {
+    state.userCheckIn = false;
+  },
+
+  IS_ONBREAK(state) {
+    state.userBreak = true;
+  },
+
+};
+
+export const actions = {
+
+  onAuthStateChangedAction: (ctx, { authUser, claims }) => {
+    if (!authUser) {
+      console.log("no USER");
+    } else {
+    }
+  },
+  
   checkedUser({ commit }) {
     commit("IS_CHECKEDIN");
   },
-});
+
+  userBreak({ commit }) {
+    commit("IS_ONBREAK");
+  },
+
+};
+
+
