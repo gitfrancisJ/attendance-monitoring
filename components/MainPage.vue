85e947283d816d055cf7bbfd51ffb22d9c2aba78
gitfrancisJ <pelaezfrancisjohn@gmail.com>
2022-11-24 16:24:08 +0800

    imported mapGetters from store and adding it into the computed property

--- components/MainPage.vue
+++ components/MainPage.vue
@@ -1,5 +1,14 @@
 <template>
-    <div class="flex items-center flex-col justify-center h-screen gap-10">
-        <TimeLogs />
-    </div>
+  <div class="relative flex items-center flex-col justify-center h-screen gap-10">
+    <AppModal v-show = 'isOnBreak'/>
+    <TimeLogs />
+  </div>
 </template>
+<script>
+import { mapGetters } from 'vuex';
+  export default {
+    computed: {
+      ...mapGetters(['isOnBreak'])
+    }
+  }
+</script>
